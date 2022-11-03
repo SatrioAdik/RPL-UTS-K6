d138403a5a8028d90f651047020ae6c08203244d
Satrio Adik Anindyo <92450379+SatrioAdik@users.noreply.github.com>
2022-11-03 20:01:10 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/utsrpl/anggota2.java
@@ -0,0 +1,14 @@
+package com.example.utsrpl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class anggota2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anggota2);
+    }
+}
